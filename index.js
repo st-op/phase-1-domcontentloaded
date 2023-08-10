2210e2b64a0d77f282aa3c521cfc500278c9384a
Stella Johansson <stellajoh9@gmail.com>
2023-08-10 16:15:53 +0200

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code goes here
+let document
+document.addEventListener('DOMContentLoaded', () => {
+    let paragraph = document.getElementById('text')
+    paragraph.textContent = 'This is really cool!'
+});
+
+function removeElement() {
+    const text = document.querySelector('p').remove();
+    const newText = document.createElement('p');
+    newText.textContent = "This is really cool!"
+}   
+function addElement() {
+    // create a new div element
+    const newDiv = document.createElement("div");
+  
+    // and give it some content
+    const newContent = document.createTextNode("This is really cool!!");
+  
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+  
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+  }
+console.log(addElement())
+
